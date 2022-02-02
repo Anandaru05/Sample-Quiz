6f404b68a96d7d08c642454a9804b814e5f26743
Anandaru05 <83396977+Anandaru05@users.noreply.github.com>
2022-02-02 17:30:10 +0530

    Add files via upload

---
+++ Quiz App/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: " HCF of 8, 9, 25 is?",
+    answer: "1",
+    options: [
+      "8",
+      "25",
+      "9",
+      "1"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The product of a rational and irrational number is?",
+    answer: "irrational",
+    options: [
+      "rational",
+      "irrational",
+      "both of above",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is the first calculating device?",
+    answer: "Abacus",
+    options: [
+       "Abacus",
+       "Calculator",
+       "Turing Machine",
+       "Pascaline"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who invented mechanical calculator called Pascaline?",
+    answer: "Blaise Pascal",
+    options: [
+      "Charles Babbage",
+      "Blaise Pascal",
+      "Alan Turing",
+      "Lee De Forest"
+    ]
+  },
+];
